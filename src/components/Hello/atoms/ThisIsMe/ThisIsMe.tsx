4203210f979dcb3024b41171ddfcf3ba065d39d0
Gustav Fredriksson <gustav.fredriksson@tink.se>
2020-12-22 13:21:33 +0100

    chore: fix a/an typo; alter some copy

--- src/components/Hello/atoms/ThisIsMe/ThisIsMe.tsx
+++ src/components/Hello/atoms/ThisIsMe/ThisIsMe.tsx
@@ -24,14 +24,15 @@ export const ThisIsMe: React.FC = () => {
   return (
     <StyledText>
       <p>
-        My name is Gustav Fredriksson and I am a engineer. I am currently
-        working as a Software Engineer at <a href="https://tink.com/">Tink</a>,
-        building infrastructure for financial services of the future.
+        My name is Gustav Fredriksson and I am an engineer. Currently, I am on a
+        mission to build the infrastructure of financial services of our
+        future–working as a Senior Software Engineer at
+        <a href="https://tink.com/">Tink</a>.
       </p>
       <p>
-        If you land on this page, you probably feel lost–despite the fact that I
-        am not looking for a new opportunity–I will leverage this opportunity
-        and market{" "}
+        If you land on this page, you might feel lost–despite the fact that I am
+        not looking for a new opportunity–I will leverage this opportunity and
+        market
         <a href="https://www.linkedin.com/in/gustav-fredriksson-98549313a">
           my resume
         </a>
