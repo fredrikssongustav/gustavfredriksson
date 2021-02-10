53b5ece5063362db33f65f3193379d9c5a6d68d3
Gustav Fredriksson <gustav.fredriksson@tink.se>
2021-02-10 20:39:13 +0100

    feat: add elevator-pitch endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,31 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const express = require("express");
+const cors = require("cors");
 
 admin.initializeApp(functions.config().firebase);
-const app = express();
 
-app.get("/hello", (req, res) => {
-  res.send("world");
+const db = admin.firestore();
+
+const app = express();
+app.use(cors({origin: "https://gustavfredriksson.com"}));
+
+app.get("/elevator-pitch/:elevatorPitchId", async (req, res) => {
+  try {
+    const document = db.collection("elevator-pitch")
+        .doc(req.params.elevatorPitchId);
+    const item = await document.get();
+    const response = item.data();
+
+    if (!response) {
+      return res.status(404).send();
+    }
+
+    return res.status(200).send(response);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
 });
 
 exports.app = functions.https.onRequest(app);
