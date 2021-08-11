2d988e47f189a7347b6dd2ac5734bb4a13a6aaee
Gustav Fredriksson <gustav.fredriksson@tink.se>
2021-08-11 16:58:45 +0200

    fix: implement tracking correctly 4 real

--- src/components/atoms/useTrackPage/useTrackPage.tsx
+++ src/components/atoms/useTrackPage/useTrackPage.tsx
@@ -1,12 +1,6 @@
 import { useEffect } from "react";
 import ReactGA from "react-ga";
 
-export const initGA = (trackingID: string) => {
-  ReactGA.initialize(trackingID);
-};
-
-initGA("G-6PE98CRQHF");
-
 export const useTrackPage = () => {
   useEffect(() => {
     ReactGA.pageview(window.location.pathname);
