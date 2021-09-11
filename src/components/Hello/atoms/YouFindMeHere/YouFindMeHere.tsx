f254ff6f54ad23533a4fa7ec8bccc53d7fcd442c
Gustav Fredriksson <gustav.fredriksson@tink.se>
2021-09-11 09:57:53 +0200

    feat: add spotify url

--- src/components/Hello/atoms/YouFindMeHere/YouFindMeHere.tsx
+++ src/components/Hello/atoms/YouFindMeHere/YouFindMeHere.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-const GithubSVG = () => (
+const GithubSvg = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="24"
@@ -12,7 +12,7 @@ const GithubSVG = () => (
   </svg>
 );
 
-const StackOverflowSVG = () => (
+const StackOverflowSvg = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="24"
@@ -23,7 +23,7 @@ const StackOverflowSVG = () => (
   </svg>
 );
 
-const LinkedInSVG = () => (
+const LinkedInSvg = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="24"
@@ -34,7 +34,7 @@ const LinkedInSVG = () => (
   </svg>
 );
 
-const MailSVG = () => (
+const MailSvg = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="24"
@@ -45,27 +45,43 @@ const MailSVG = () => (
   </svg>
 );
 
+const SpotifySvg = () => (
+  <svg
+    width="24"
+    height="24"
+    xmlns="http://www.w3.org/2000/svg"
+    fillRule="evenodd"
+    clipRule="evenodd"
+  >
+    <path d="M19.098 10.638c-3.868-2.297-10.248-2.508-13.941-1.387-.593.18-1.22-.155-1.399-.748-.18-.593.154-1.22.748-1.4 4.239-1.287 11.285-1.038 15.738 1.605.533.317.708 1.005.392 1.538-.316.533-1.005.709-1.538.392zm-.126 3.403c-.272.44-.847.578-1.287.308-3.225-1.982-8.142-2.557-11.958-1.399-.494.15-1.017-.129-1.167-.623-.149-.495.13-1.016.624-1.167 4.358-1.322 9.776-.682 13.48 1.595.44.27.578.847.308 1.286zm-1.469 3.267c-.215.354-.676.465-1.028.249-2.818-1.722-6.365-2.111-10.542-1.157-.402.092-.803-.16-.895-.562-.092-.403.159-.804.562-.896 4.571-1.045 8.492-.595 11.655 1.338.353.215.464.676.248 1.028zm-5.503-17.308c-6.627 0-12 5.373-12 12 0 6.628 5.373 12 12 12 6.628 0 12-5.372 12-12 0-6.627-5.372-12-12-12z" />
+  </svg>
+);
+
 const Container = styled.div`
   display: flex;
   justify-content: space-between;
-  margin: 30px 15%;
-  padding-bottom: 100px;
+  padding: 0 5%;
+  padding-top: 30px;
+  margin-bottom: 100px;
 `;
 
 export const YouFindMeHere: React.FC = () => {
   return (
     <Container>
       <a href="https://github.com/fredrikssongustav">
-        <GithubSVG />
+        <GithubSvg />
       </a>
       <a href="https://stackoverflow.com/users/10600871/gqstav">
-        <StackOverflowSVG />
+        <StackOverflowSvg />
       </a>
       <a href="https://www.linkedin.com/in/gustav-fredriksson-98549313a">
-        <LinkedInSVG />
+        <LinkedInSvg />
       </a>
       <a href="mailto:gustav_fredriksson@live.se">
-        <MailSVG />
+        <MailSvg />
+      </a>
+      <a href="https://open.spotify.com/playlist/0PSMBtTl06wgBFD5TNyR8f">
+        <SpotifySvg />
       </a>
     </Container>
   );
