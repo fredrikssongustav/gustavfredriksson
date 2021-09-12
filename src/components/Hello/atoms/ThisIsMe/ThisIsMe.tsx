7911f9891a9c475d1e78bad76f8a8c7a99399950
Gustav Fredriksson <gustav.fredriksson@tink.se>
2021-09-12 10:11:20 +0200

    chore: update cringe copy

--- src/components/Hello/atoms/ThisIsMe/ThisIsMe.tsx
+++ src/components/Hello/atoms/ThisIsMe/ThisIsMe.tsx
@@ -5,13 +5,13 @@ export const ThisIsMe: React.FC = () => {
     <div style={{ textAlign: "justify" }}>
       <p>
         My name is Gustav and I bend software. Currently, I am on a mission to
-        build the infrastructure of financial services of our future–working as
-        a Senior Software Engineer at <a href="https://tink.com/">Tink</a>.
+        build the infrastructure for the financial services of our
+        future–working as a Senior Software Engineer at{" "}
+        <a href="https://tink.com/">Tink</a>.
       </p>
       <p>
-        If you land on this page that makes me glad–despite the fact that I am
-        not looking for a new opportunity–I will leverage this opportunity and
-        promote{" "}
+        I am not looking for a new opportunity but will regardless leverage this
+        opportunity and promote{" "}
         <a href="https://www.linkedin.com/in/gustav-fredriksson-98549313a">
           my resume
         </a>
