c1c22503f05850cb50e1f5d1be25ffcd183c81c0
Gustav Fredriksson <gustav.fredriksson@atlar.com>
2024-09-14 17:57:14 +0200

    chore: delete the old react app
    Everything is deprecated

--- src/components/atoms/Title/Title.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Title = styled.h1`
-  font-size: ${(props) => props.theme.sizes.monumental};
-  text-align: left;
-  margin: 0;
-  line-height: ${(props) => props.theme.sizes.monumental};
-`;
